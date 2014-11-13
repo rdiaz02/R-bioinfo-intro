d08e82e95115c0d53d51358b8fea195f29e2af1a
ramon diaz-uriarte (at Bufo) <rdiaz02@gmail.com>
2014-11-13 23:52:19 +0100

    producing HTML too

---
+++ make-html.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+FILE=$1
+BASENAME=$(basename $FILE .Rnw)
+RSCRIPT="/usr/bin/Rscript"
+BASENAME2=$BASENAME-knitr-html
+FILE2=$BASENAME2.Rnw
+
+cp $FILE $FILE2
+sed -i 's/^%%listings-knitr-html%%//' $FILE2
+$RSCRIPT -e 'library(knitr); knit("'$FILE2'")'
+
+sweave2html $BASENAME2
+mv $BASENAME2.html $BASENAME.html
+
+mkdir $BASENAME-html-dir
+cp $BASENAME.html ./$BASENAME-html-dir/.
+cp -a figure ./$BASENAME-html-dir/.
+zip -r $BASENAME-html-dir.zip $BASENAME-html-dir
+
+## sweave2html from http://biostat.mc.vanderbilt.edu/wiki/Main/SweaveConvert#Converting_from_LaTeX_to_html  
