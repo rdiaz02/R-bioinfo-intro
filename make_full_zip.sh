33aff76bc2ef1e4bb6446944a971f6092383af7a
ramon diaz-uriarte (at Bufo) <rdiaz02@gmail.com>
2014-11-15 17:20:40 +0100

    seems ready for v.1.0?

---
+++ make_full_zip.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+runKnitr.sh R-bioinfo-intro.Rnw
+make-html.sh R-bioinfo-intro.Rnw
+zip -r Additional_files_R-bioinfo-intro.zip\
+ hit-table-500-text.txt AnotherDataSet.txt\
+ anage.RData lastExample.R Condition_A.txt\
+ Condition_B.txt Condition_C.txt R-bioinfo-intro.R
