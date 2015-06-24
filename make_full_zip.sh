065a4d6d0b0fd724f49275583934658e46a64f78
Ramon Diaz-Uriarte (at Coleonyx) <rdiaz02@gmail.com>
2015-06-24 18:57:51 +0200

    zip creation: there were missing files

--- make_full_zip.sh
+++ make_full_zip.sh
@@ -3,7 +3,10 @@ runKnitr.sh R-bioinfo-intro.Rnw
 
 ./make-html.sh R-bioinfo-intro.Rnw
 
+zip -r permafrost.zip permafrost
+
 zip -r Additional_files_R-bioinfo-intro.zip\
  hit-table-500-text.txt AnotherDataSet.txt\
  anage.RData lastExample.R Condition_A.txt\
+ script1.R permafrost.zip\
  Condition_B.txt Condition_C.txt R-bioinfo-intro.R
