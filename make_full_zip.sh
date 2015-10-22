a395dfe0ebfa815f84611235ac7ed5580527ab3a
Ramon Diaz-Uriarte (at Coleonyx) <rdiaz02@gmail.com>
2015-10-22 14:53:35 +0200

    runKnitr2 added and no longer creating html

--- make_full_zip.sh
+++ make_full_zip.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
-runKnitr.sh R-bioinfo-intro.Rnw
+runKnitr-2.sh R-bioinfo-intro.Rnw purl
 
-./make-html.sh R-bioinfo-intro.Rnw
+## ./make-html.sh R-bioinfo-intro.Rnw
 
-zip -r permafrost.zip permafrost
+## zip -r permafrost.zip permafrost
+
+# zip -r Additional_files_R-bioinfo-intro.zip\
+#  hit-table-500-text.txt AnotherDataSet.txt\
+#  anage.RData lastExample.R Condition_A.txt\
+#  script1.R permafrost.zip\
+#  Condition_B.txt Condition_C.txt R-bioinfo-intro.R
 
 zip -r Additional_files_R-bioinfo-intro.zip\
  hit-table-500-text.txt AnotherDataSet.txt\
  anage.RData lastExample.R Condition_A.txt\
- script1.R permafrost.zip\
+ script1.R\
  Condition_B.txt Condition_C.txt R-bioinfo-intro.R
+
+
+## runKnitr-2.sh is in the repo
