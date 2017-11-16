d4856f0d7d4dc7323efc9025fb3dc736b8510b28
ramon diaz-uriarte (at Phelsuma) <rdiaz02@gmail.com>
2017-11-16 13:03:00 +0100

    more on the figure example

--- Metabolic-rate-example.R
+++ Metabolic-rate-example.R
@@ -107,6 +107,7 @@ p2 + scale_x_log10() + scale_y_log10() +
 
 library(cowplot)
 
+#### Aqui nos quedamos
 
 ## selection
 
@@ -136,3 +137,140 @@ legend(locator(1), legend = levels(anage$Class),
 ## - labels con at para etiquetar tick marks
 ## complete cases y table sin NAs
 ## abline para plot para reptiles y aves
+
+
+
+plot(logmet ~ logbm, data = anage,
+     xlab = "Log body mass (g)", ylab = "Log metabolic rate (W)",
+     axes = FALSE)
+box()
+
+## help for labels is not very helpful?
+## where?
+
+summary(anage)
+exp(seq(from = -4.3, to = 2, length.out = 6))
+
+yv <- c(0.01, 0.05, 0.15, 0.6, 2, 7) 
+log(yv)
+
+axis(side = 2, at = log(yv), labels = yv)
+
+## Hummm, ugly for several reasons
+
+par(las = 1)
+plot(logmet ~ logbm, data = anage,
+     xlab = "Log body mass (g)", ylab = "Log metabolic rate (W)",
+     axes = FALSE, ylim = c(log(0.01), max(anage$logmet, na.rm = TRUE)))
+box()
+axis(side = 2, at = log(yv), labels = yv)
+
+dev.off()
+
+op <- par(las = 1)
+
+plot(logmet ~ logbm, data = anage,
+     xlab = "Log body mass (g)", ylab = "Log metabolic rate (W)",
+     axes = FALSE, ylim = c(log(0.01), max(anage$logmet, na.rm = TRUE)))
+box()
+axis(side = 2, at = log(yv), labels = yv)
+
+par(op)
+
+
+anage.clean <- anage[
+    complete.cases(anage[, c("Class", "Metabolic.rate..W.", "Body.mass..g.")]), ]
+summary(anage.clean)
+
+anage.clean2 <- na.omit(
+    anage[, c("Class", "Metabolic.rate..W.", "Body.mass..g.")])
+summary(anage.clean2)
+
+anage3 <- dplyr::filter(anage, !is.na(Metabolic.rate..W.) & !is.na(Body.mass..g.))
+anage4 <- dplyr::filter(anage, !(is.na(Metabolic.rate..W.) | is.na(Body.mass..g.)))
+summary(anage3)
+summary(anage4)
+identical(anage3, anage4)
+
+## Hummm...
+identical(anage.clean, anage3)
+
+mapply(identical, anage.clean, anage3)
+## what gives?
+
+attributes(anage.clean)
+attributes(anage3)
+
+## misleading
+mapply(identical, attributes(anage.clean), attributes(anage3))
+names(attributes(anage3))
+names(attributes(anage.clean))
+
+for(att in names(attributes(anage3))) {
+    cat("\n attribute ", att, "\n")
+    print(identical(attributes(anage)[[att]], attributes(anage.clean)[[att]]))
+}
+
+
+### ablines and regression
+
+birds <- dplyr::filter(anage, Class == "Aves")
+
+summary(lm(logmet ~ logbm, birds))
+
+plot(logmet ~ logbm, birds)
+abline(lm(logmet ~ logbm, birds))
+
+## ok but ...
+
+mbirds <- lm(logmet ~ logbm, data = dplyr::filter(anage, Class == "Aves"))
+## similar to
+mbirds0 <- lm(logmet ~ logbm, data = anage, subset = (Class == "Aves"))
+
+mrept <- lm(logmet ~ logbm, data = dplyr::filter(anage, Class != "Aves"))
+
+colors()
+
+plot(logmet ~ logbm, anage, col = c("salmon", "turquoise")[Class])
+abline(mbirds, col = "salmon")
+abline(mrept, col = "turquoise")
+
+
+
+## nicer, smarter?
+
+lapply(split(anage, anage$Class),
+       function(dd) lm(logmet ~ logbm, data = dd))
+
+plot(logmet ~ logbm, anage, col = c("salmon", "turquoise")[Class])
+lapply(split(anage, anage$Class),
+       function(dd) abline(lm(logmet ~ logbm, data = dd)))
+
+
+
+plot(logmet ~ logbm, anage, col = c("salmon", "turquoise")[Class])
+lapply(split(anage, anage$Class),
+       function(dd) abline(lm(logmet ~ logbm, data = dd)))
+
+
+colores <- c("salmon", "turquoise")
+
+## hummm...
+plot(logmet ~ logbm, anage, col = colores[Class])
+lapply(split(anage, anage$Class),
+       function(dd) abline(lm(logmet ~ logbm, data = dd,
+                              col = colores[Class])))
+
+lapply(split(anage, anage$Class),
+       function(dd) abline(lm(logmet ~ logbm, data = dd),
+                              col = colores[dd$Class]))
+
+## if we get side tracked
+dddd <- split(anage, anage$Class)
+
+lapply(dddd, function(u) u$Class)
+lapply(split(anage, anage$Class),
+       function(dd) abline(lm(logmet ~ logbm, data = dd,
+                              col = colores[Class])))
+
+
