50d3abee4d55eeaa55f83a8354832626e30a0070
ramon diaz-uriarte (at Phelsuma) <rdiaz02@gmail.com>
2018-11-29 19:30:24 +0100

    minor typos

--- plots-summary.R
+++ plots-summary.R
@@ -6,7 +6,7 @@ hit <- read.table("hit-table-500-text.txt")
 ## I want a histogram of alignment length and a scatterplot of score
 ## vs. identity
 
-par(mfrow = c(1, 2)) ## two figures side by side
+op <- par(mfrow = c(1, 2)) ## two figures side by side
 hist(hit[, 5], breaks = 50, xlab = "", main = "Alignment length")
 plot(hit[, 13] ~ hit[, 3], xlab = "Percent. identity", 
      ylab = "Bit score")
@@ -29,6 +29,10 @@ library(car) ## make the car package available; this is NOT
              ## installing it. It is making it available
 scatterplot(Metabolic.rate..W. ~ Body.mass..g., log="xy", 
             data = anage)
+
+
+
+
 
 scatterplot(Metabolic.rate..W. ~ Body.mass..g.|Class, log="xy", 
             data = anage)
