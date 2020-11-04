73b877b5e8806651d275fbf9324ed7d8e94adc95
ramon diaz-uriarte (at Phelsuma) <rdiaz02@gmail.com>
2020-11-04 10:40:17 +0100

    references to additional files

--- merge-join.R
+++ merge-join.R
@@ -3,10 +3,22 @@
 ## https://www.rstudio.com/wp-content/uploads/2015/02/data-wrangling-cheatsheet.pdf
 ## http://stat545.com/bit001_dplyr-cheatsheet.html
 
+## I tend to use data.table now. The equivalent of left and right join are
+## possible doing: e.g.,
+## https://rstudio-pubs-static.s3.amazonaws.com/52230_5ae0d25125b544caab32f75f0360e775.html)
+## https://stackoverflow.com/questions/12773822/why-does-xy-join-of-data-tables-not-allow-a-full-outer-join-or-a-left-join
+## What I do most often
+## left: merge(x, y, all.x = TRUE)
+## right: merge(x, y, all.y = TRUE)
+## full outer: merge(x, y, all = TRUE)
+
+
+
 
 ## All examples with merge from Ch. 13 of "R for dummies", 2nd ed. de
 ## Vries and Meys.
 
+## state.x77 is part of datasets, so immediately available
 all.states <- as.data.frame(state.x77)
 all.states$Name <- rownames(state.x77)
 rownames(all.states) <- NULL
